85dbcde4d7aaf21836e1a2cfac0559383f7b3dfe
Carlos Espinoza <cpespinoza044@gmail.com>
2024-12-12 18:44:19 -0600

    cambios con el dashboard

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -3,5 +3,6 @@ export { default as FacebookIcon } from "./icons/gen/FacebookIcon.astro";
 export { default as GithubIcon } from "./icons/gen/GithubIcon.astro";
 export { default as MenuIcon } from "./icons/gen/MenuIcon.astro";
 export { default as TwitterIcon } from "./icons/gen/TwitterIcon.astro";
+export { default as CircleIcon } from "./icons/gen/CircleIcon.astro";
 
 export { default as DashboardBlur } from "./home/DashboardBlur.astro";
