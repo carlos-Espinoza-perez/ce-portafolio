738864bfda3869ecc2e4debbeb13a251a4f4612a
Carlos Espinoza <cpespinoza044@gmail.com>
2024-12-16 19:48:19 -0600

    cambios

---
+++ src/common/Variables.ts
@@ -0,0 +1,9 @@
+const VARIABLES = {
+  redes: {
+    twitter: "https://x.com/cpespinoza044",
+    facebook: "https://www.facebook.com/carlosheberto.espinozaperez",
+    github: "https://github.com/carlos-espinoza-perez",
+  },
+};
+
+export default VARIABLES;
